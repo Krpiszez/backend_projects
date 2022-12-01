d8902410664cf5fa69971a89c5175254f0c7b873
Omer Isik <isikomer4545@gmail.com>
2022-12-01 01:01:07 +0100

    first

---
+++ src/main/java/AssignmentJava.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+public class AssignmentJava {
+    /* - Type code to ask user to enter the int elements and add elements into the list
+        - Ask user to enter the elements to remove, then remove that element from list.
+        - Ask user to enter the element to update, then update it.
+
+  */
+    public static void main(String[] args) {
+
+        updateEl(removeEl());
+
+    }
+
+    public static List<Integer> removeEl(){
+        Scanner in = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+        System.out.println("Enter your list elements, press any letter to quit insetion!");
+        while (in.hasNextInt()){
+            list.add(in.nextInt());
+        }
+        System.out.println("Your list is: " + list);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter num to be removed");
+        ListIterator<Integer> itr = list.listIterator();
+        while (scan.hasNextInt()) {
+            while (itr.hasNext()){
+                int el = itr.next();
+                if (el == scan.nextInt()){
+                    itr.remove();
+                    break;
+                }
+
+            }
+        }
+        System.out.println("Your list after removal of numbers: " + list);
+        return list;
+    }
+    public static List<Integer> updateEl(List<Integer> list){
+        ListIterator<Integer> itr = list.listIterator();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter num to update then new value press any letter to end assertion");
+        while (in.hasNextInt()){
+            while (itr.hasNext()){
+                int num = itr.next();
+                if (num == in.nextInt()){
+                    itr.set(in.nextInt());
+                }
+            }
+        }
+        System.out.println(list);
+        return list;
+    }
+
+}
