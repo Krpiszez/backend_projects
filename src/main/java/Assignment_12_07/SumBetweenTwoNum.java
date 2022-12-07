b80e47be2dd71cf4c3167e632d7870c00d93a77b
Omer Isik <isikomer4545@gmail.com>
2022-12-07 19:39:56 +0100

    first2

--- src/main/java/Assignment_12_07/SumBetweenTwoNum.java
+++ src/main/java/Assignment_12_07/SumBetweenTwoNum.java
@@ -1,20 +1,28 @@
 package Assignment_12_07;
 
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class SumBetweenTwoNum {
 
     public static void main(String[] args) {
-
-        System.out.println(new SumBetweenTwoNum().SumBetweenTwoNum(2, 4));
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter 2 value to get the sum the numbers between them");
+        int m = in.nextInt();
+        int n = in.nextInt();
+        System.out.println(new SumBetweenTwoNum().SumBetweenTwoNum(m, n));
 
     }
 
 
     public int SumBetweenTwoNum(int m, int n){
 
+        if (m>n){
+            m=m+n;
+            n=m-n;
+            m=m-n;
+        }
         return IntStream.rangeClosed(m,n).sum();
-
     }
 
 }
