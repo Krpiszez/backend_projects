563aba762ed806563a42d3055135f2929635223d
Omer Isik <isikomer4545@gmail.com>
2022-12-22 15:05:59 +0100

    second

---
+++ src/main/java/Assignment_22_12/Library.java
@@ -0,0 +1,11 @@
+package Assignment_22_12;
+
+public class Library {
+    /*
+         2. Write the program that stores information about university library users: surname and initials of the reader,
+        the number of books on the subscription. It has to be possible to add information about library users manually.
+        On the user request the program must display either a message about the reader who has the largest number of books,
+        or a list of library users in ascending order based on the number of books (6 points)
+     */
+
+}
