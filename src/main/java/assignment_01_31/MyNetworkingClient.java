87fef9323237fb969c3e4f6a31f4d7059aa31824
Omer Isik <isikomer4545@gmail.com>
2023-01-31 15:00:03 +0100

    last

--- src/main/java/assignment_01_31/MyNetworkingClient.java
+++ src/main/java/assignment_01_31/MyNetworkingClient.java
@@ -1,4 +1,34 @@
 package assignment_01_31;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class MyNetworkingClient {
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 8000)) {
+            System.out.println("The Client has been Connected!");
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter first number: ");
+            int num1 = sc.nextInt();
+
+            System.out.print("Enter second number: ");
+            int num2 = sc.nextInt();
+            // On the client side, the user is prompted to enter two numbers,
+            // which are then sent to the server.
+            dos.writeInt(num1);
+            dos.writeInt(num2);
+            //The result of the calculation is then displayed to the user.
+            int result = dis.readInt();
+            System.out.println("Result from server: " + result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
--- src/main/java/assignment_01_31/MyNetworkingServer.java
+++ src/main/java/assignment_01_31/MyNetworkingServer.java
@@ -1,4 +1,31 @@
 package assignment_01_31;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MyNetworkingServer {
+    public static void main(String[] args) {
+        //Here, the server listens on port 8000 and waits for a client to connect.
+                try (ServerSocket serverSocket = new ServerSocket(8000)) {
+                    System.out.println("The Server has been Started!");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                DataInputStream dis = new DataInputStream(socket.getInputStream());
+                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        //When a client connects, it reads two integers from the client and returns
+                // the sum of those two integers back to the client.
+                int num1 = dis.readInt();
+                int num2 = dis.readInt();
+                int result = num1 + num2;
+
+                dos.writeInt(result);
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
