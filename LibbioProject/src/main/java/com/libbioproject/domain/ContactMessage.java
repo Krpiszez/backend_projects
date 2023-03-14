c4264dc204b42605a85dd450a96f519a59ef935b
Omer Isik <isikomer4545@gmail.com>
2023-03-14 20:52:44 +0100

    New Library Project

---
+++ LibbioProject/src/main/java/com/libbioproject/domain/ContactMessage.java
@@ -0,0 +1,22 @@
+package com.libbioproject.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "t_contact_message")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContactMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
---
+++ LibbioProject/src/test/java/com/libbioproject/LibbioProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libbioproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibbioProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
