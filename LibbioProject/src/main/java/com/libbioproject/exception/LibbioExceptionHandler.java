f006649e561d26a74e4dc5f59b1d34a94446b23e
Omer Isik <isikomer4545@gmail.com>
2023-04-16 01:32:02 +0200

    Some modifications in pom.xml

--- LibbioProject/src/main/java/com/libbioproject/exception/LibbioExceptionHandler.java
+++ LibbioProject/src/main/java/com/libbioproject/exception/LibbioExceptionHandler.java
@@ -4,13 +4,13 @@ import com.libbioproject.exception.message.ApiResponseError;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.naming.AuthenticationException;
 import java.nio.file.AccessDeniedException;
 import java.util.List;
 import java.util.stream.Collectors;
