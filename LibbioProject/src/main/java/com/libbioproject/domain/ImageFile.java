982cc4f914505bde87a535f6163b74853576d2fc
Omer Isik <isikomer4545@gmail.com>
2023-04-24 00:20:44 +0200

    Image File

--- LibbioProject/src/main/java/com/libbioproject/domain/ImageFile.java
+++ LibbioProject/src/main/java/com/libbioproject/domain/ImageFile.java
@@ -22,10 +22,25 @@ public class ImageFile {
     private String id;
     private String name;
     private String type;
-    private long length;
+    private long imageLength;
+    private long contentLength;
     @OneToOne(cascade = CascadeType.ALL)
     private CoverImage coverImage;
     @OneToOne(cascade = CascadeType.ALL)
     private BookContent bookContent;
+
+    public ImageFile (String name, String type, CoverImage coverImage){
+        this.name=name;
+        this.type=type;
+        this.coverImage = coverImage;
+        this.imageLength = coverImage.getImageData().length;
+    }
+
+    public ImageFile (String name, String type, BookContent bookContent){
+        this.name=name;
+        this.type=type;
+        this.bookContent = bookContent;
+        this.contentLength = bookContent.getContentData().length;
+    }
 
 }
