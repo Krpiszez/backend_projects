12b6149c6eb0a93266c1577fd3b5b690edfac12d
Omer Isik <isikomer4545@gmail.com>
2023-06-24 23:49:42 +0200

    enum strategy dp - switch op

---
+++ EffectiveJava3Edition/src/main/java/enums_strategy_dp/OperationSwitch.java
@@ -0,0 +1,16 @@
+package enums_strategy_dp;
+
+public enum OperationSwitch {
+
+    PLUS,MINUS,TIMES,DIVIDE;
+
+    public double apply(double x, double y){
+        switch (this){
+            case PLUS : return x+y;
+            case MINUS: return x-y;
+            case TIMES: return x*y;
+            case DIVIDE: return x/y;
+        }
+        throw new AssertionError("Unknown op: " + this);
+    }
+}
