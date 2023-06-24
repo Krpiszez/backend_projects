91ccb81855fc20440aaa9f697ec6154e2cbc0fe0
Omer Isik <isikomer4545@gmail.com>
2023-06-24 23:44:21 +0200

    Enums

--- EffectiveJava3Edition/src/main/java/enums_strategy_dp/Operations.java
+++ EffectiveJava3Edition/src/main/java/enums_strategy_dp/Operations.java
@@ -5,12 +5,33 @@ public enum Operations {
     PLUS("+"){
         public double apply(double x, double y){return x+y;}
     },
+    MINUS("-"){
+        public double apply(double x, double y){return x-y;}
+    },
+    TIMES("*") {
+        public double apply(double x, double y) {
+            return x * y;
+        }
+    },
+    DIVIDE("/") {
+        public double apply(double x, double y) {
+            return x / y;
+        }
+    };
 
+    @Override
+    public String toString() {
+        return "Operations{" +
+                "symbol='" + symbol + '\'' +
+                '}';
+    }
 
     private final String symbol;
 
     Operations(String symbol){
         this.symbol = symbol;
     }
+
+    public abstract double apply(double x, double y);
 
 }
