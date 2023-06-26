6f0f6096c60505dbe8ace36ad5b68084b3fdb59f
Omer Isik <isikomer4545@gmail.com>
2023-06-26 21:12:42 +0200

    EnumSet usage

---
+++ EffectiveJava3Edition/src/main/java/enumset_bitwise/Text.java
@@ -0,0 +1,25 @@
+package enumset_bitwise;
+
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+
+//EnumSet - a modern replacement for bit fields (Page 170)
+public class Text {
+	
+	public enum Style {
+		BOLD, ITALIC, UNDERLINE, STRIKETHROUGH
+	}
+
+	// Any Set could be passed in, but EnumSet is clearly best
+	public void applyStyles(Set<Style> styles) {
+		System.out.printf("Applying styles %s to text%n", Objects.requireNonNull(styles));
+	}
+
+	// Sample use
+	public static void main(String[] args) {
+		Text text = new Text();
+		text.applyStyles(EnumSet.of(Style.BOLD, Style.ITALIC, Style.BOLD));
+	}
+	
+}
