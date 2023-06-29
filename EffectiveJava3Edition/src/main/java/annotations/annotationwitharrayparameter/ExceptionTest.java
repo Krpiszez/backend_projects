fefba6bb07bcc52bb3baa3bd3617641d07fe4693
Omer Isik <isikomer4545@gmail.com>
2023-06-29 19:24:14 +0200

    annotation with parameter as array

---
+++ EffectiveJava3Edition/src/main/java/annotations/annotationwitharrayparameter/ExceptionTest.java
@@ -0,0 +1,13 @@
+package annotations.annotationwitharrayparameter;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//Annotation type with an array parameter  (Page 184)
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ExceptionTest {
+	Class<? extends Exception>[] value();
+}
