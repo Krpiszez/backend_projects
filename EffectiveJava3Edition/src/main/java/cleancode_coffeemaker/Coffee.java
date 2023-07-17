73134570b1a2e4f9ac14a1c335b8f4b85b779819
Omer Isik <isikomer4545@gmail.com>
2023-07-17 23:20:48 +0200

    A builder class added!

--- EffectiveJava3Edition/src/main/java/cleancode_coffeemaker/Coffee.java
+++ EffectiveJava3Edition/src/main/java/cleancode_coffeemaker/Coffee.java
@@ -1,4 +1,31 @@
 package cleancode_coffeemaker;
 
 public class Coffee {
+
+    private String quality;
+    private int amount;
+
+    public Coffee(String quality, int amount) {
+        this.quality = quality;
+        this.amount = amount;
+    }
+
+    public Coffee() {
+    }
+
+    public static class Builder{
+
+        private String quality;
+        private int amount;
+
+        public Builder twoCups(){
+            this.amount = 2;
+            return this;
+        }
+    }
+
+    public Coffee(Builder builder){
+        this.quality = builder.quality;
+        this.amount = builder.amount;
+    }
 }
