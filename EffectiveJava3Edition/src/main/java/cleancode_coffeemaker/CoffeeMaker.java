3bd53edae3294fa61b58644785a5e104c13d625a
Omer Isik <isikomer4545@gmail.com>
2023-07-18 21:29:55 +0200

    With milk

--- EffectiveJava3Edition/src/main/java/cleancode_coffeemaker/CoffeeMaker.java
+++ EffectiveJava3Edition/src/main/java/cleancode_coffeemaker/CoffeeMaker.java
@@ -2,11 +2,84 @@ package cleancode_coffeemaker;
 
 public class CoffeeMaker implements ICoffeeMaker{
 
+    private Coffee coffee;
     private String sugar;
     private String milk;
     private String chocolate;
+    private int quality;
+    private int quantity;
+
+    public Coffee getCoffee() {
+        return coffee;
+    }
+
+    public void setCoffee(Coffee coffee) {
+        this.coffee = coffee;
+    }
+
+    public String getSugar() {
+        return sugar;
+    }
+
+    public void setSugar(String sugar) {
+        this.sugar = sugar;
+    }
+
+    public String getMilk() {
+        return milk;
+    }
+
+    public void setMilk(String milk) {
+        this.milk = milk;
+    }
+
+    public String getChocolate() {
+        return chocolate;
+    }
+
+    public void setChocolate(String chocolate) {
+        this.chocolate = chocolate;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
     @Override
     public Coffee brewCoffee() {
         return null;
     }
+
+    public static class CoffeeBuilder{
+        private Coffee coffee;
+        private String sugar;
+        private String milk;
+        private String chocolate;
+        private int quality;
+        private int quantity;
+
+        public CoffeeBuilder(int quality, int quantity) {
+            this.quality = quality;
+            this.quantity = quantity;
+        }
+
+        public CoffeeBuilder withMilk(String milk){
+            this.milk = milk;
+            return this;
+        }
+    }
+
+
 }
