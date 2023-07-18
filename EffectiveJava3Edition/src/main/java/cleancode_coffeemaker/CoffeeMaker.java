fa9d068f064f5c34f25299a8f1f7f0d8ba306176
Omer Isik <isikomer4545@gmail.com>
2023-07-18 21:25:00 +0200

    Maker getting ready

---
+++ EffectiveJava3Edition/src/main/java/cleancode_coffeemaker/CoffeeMaker.java
@@ -0,0 +1,12 @@
+package cleancode_coffeemaker;
+
+public class CoffeeMaker implements ICoffeeMaker{
+
+    private String sugar;
+    private String milk;
+    private String chocolate;
+    @Override
+    public Coffee brewCoffee() {
+        return null;
+    }
+}
